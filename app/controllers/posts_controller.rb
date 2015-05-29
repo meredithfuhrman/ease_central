d8f153ca8a4ebe8c7ef75a6c489a701a2d4a2026
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-05-28 22:23:37 -0400

    Got top links from JSON to show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  def index
+    response = Net::HTTP.get_response(URI.parse("http://www.reddit.com/top.json"))
+    result = JSON.parse(response.body)["data"]["children"]
+
+    @posts = []
+    result.each do |post|
+      post.each do |key, value|
+        if key == "data"
+          @posts << {title: value["title"], link: value["permalink"],
+          num_comments: value["num_comments"], media_embed: value["media_embed"] }
+        end
+      end
+    end
+  end
+
+end
